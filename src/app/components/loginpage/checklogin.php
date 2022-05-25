803e527f984c53bc0270d34a8245f08e4f0884d6
juliomc23 <julito2302mc@gmail.com>
2022-05-25 09:56:30 +0200

    i dont know what i did but it works

--- src/app/components/loginpage/checklogin.php
+++ src/app/components/loginpage/checklogin.php
@@ -1,37 +1,5 @@
 <?php 
-    function checkUser()
-    {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-    
-        $data = json_decode(file_get_contents('../../../db/db.json'), true);
-    
-        foreach ($data as $user) {
-    
-            $encPassword = password_hash($user['password'], PASSWORD_DEFAULT);
-            if ($user['email'] === $email) {
-    
-                if (password_verify($password, $encPassword)) {
-                    session_start();
-                    $_SESSION['email'] = $email;
-                    $_SESSION['password'] = $password;
-                    $_SESSION['firstname'] = $user['firstName'];
-                    $_SESSION['lastname'] = $user['lastName'];
-                    $_SESSION['img'] = $user['img'];
-                    $_SESSION['lastloggedIn'] = $user['lastLoggedIn'];
-                    $_SESSION['pathUser'] = $user['root'];
-
-                    echo "hemos entrado";
-
-                    header('Location: ../../../../index.php');
-                 } else {
-                    header('Location: ./login.php?passworderror=true');
-                }
-            } else {
-                header('Location: ./login.php?usererror=true');
-            }
-        }
-    }
+    require('../../../modules/functions.php');
 
     checkUser();
 
--- src/modules/functions.php
+++ src/modules/functions.php
@@ -1,7 +1,39 @@
 <?php
+function checkUser()
+{
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $data = json_decode(file_get_contents('../../../db/db.json'), true);
+
+    foreach ($data as $user) {
+
+        $encPassword = password_hash($user['password'], PASSWORD_DEFAULT);
+        if ($user['email'] === $email) {
 
 
+            if (password_verify($password, $encPassword)) {
 
+                var_dump(password_verify($password, $encPassword));
+
+                            session_start();
+                            $_SESSION['email'] = $email;
+                            $_SESSION['password'] = $password;
+                            $_SESSION['firstname'] = $user['firstName'];
+                            $_SESSION['lastname'] = $user['lastName'];
+                            $_SESSION['img'] = $user['img'];
+                            $_SESSION['lastloggedIn'] = $user['lastLoggedIn'];
+                            $_SESSION['pathUser'] = $user['root'];
+
+                            header('Location: ../../../../index.php');
+                        } else {
+                            header('Location: ./login.php?passworderror=true');
+                        }
+            } else {
+                header('Location: ./login.php?usererror=true');
+        }
+    }
+}
 
 function checkSession()
 {
