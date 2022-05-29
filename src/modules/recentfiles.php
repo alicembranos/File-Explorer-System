f74591d26959a036bfb73935136d24c3f2f5a88e
Alicia Cembranos <alicembru15@gmail.com>
2022-05-29 22:38:42 +0200

    css style search bar and recent files

--- src/modules/recentfiles.php
+++ src/modules/recentfiles.php
@@ -18,14 +18,15 @@ $_SESSION["recentFiles"] = getFirstElementsArray(5, $arraySort);
     array_pop($_SESSION["recentFiles"]);  //delete last element
 ?>
     <?php foreach ($_SESSION["recentFiles"] as $recentFile) : ?>
-        <article class="card shadow p-1 mb-5 bg-white" style="width: 15rem;">
+        <article class="card-recent bg-white" style="width: 15rem;">
             <p class="card-img"><?= getImageExtension($recentFile) ?></p>
             <!-- <img src="" class="card-img-top" alt="avi-file-img" style="width: 48px;"> -->
             <div class="card-body">
-                <h5 class="card-title"><?= getNameFile($rootUserPath, $recentFile) ?></h5>
+            <a class ="title__link" href="<?= $recentFile ?>">
+                    <h5 class="card-title"><?= getNameFile($rootUserPath, $recentFile) ?></h5>
+                </a>
                 <p class="card-text"><?= getSizeFile($rootUserPath, $recentFile) ?></p>
                 <div class="card__actionbuttons">
-                    <button class="card__icon"><i class="fa-solid fa-download"></i></button>
                     <button class="card__icon"><i class="fa-solid fa-pencil"></i></button>
                     <button class="card__icon"><i class="fa-solid fa-trash"></i></button>
                 </div>
@@ -34,16 +35,15 @@ $_SESSION["recentFiles"] = getFirstElementsArray(5, $arraySort);
     <?php endforeach ?>
 <?php else : ?>
     <?php foreach ($_SESSION["recentFiles"] as $recentFile) : ?>
-        <article class="card shadow p-1 mb-5 bg-white" style="width: 15rem;">
+        <article class="card-recent bg-white" style="width: 15rem;">
             <img src="<?= getImageExtension($recentFile) ?>" class="card-img" alt="avi-file-img" style="width: 48px;">
             <div class="card-body">
                 <!-- <h5 class="card-title"><?= getNameFile($rootUserPath, $recentFile) ?></h5> -->
-                <a href="<?= $recentFile ?>">
+                <a class ="title__link" href="<?= $recentFile ?>">
                     <h5 class="card-title"><?= getNameFile($rootUserPath, $recentFile) ?></h5>
                 </a>
                 <p class="card-text"><?= getSizeFile($rootUserPath, $recentFile) ?></p>
                 <div class="card__actionbuttons">
-                    <button class="card__icon"><i class="fa-solid fa-download"></i></button>
                     <button class="card__icon"><i class="fa-solid fa-pencil"></i></button>
                     <button class="card__icon"><i class="fa-solid fa-trash"></i></button>
                 </div>
