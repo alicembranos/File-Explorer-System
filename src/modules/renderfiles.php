29d4369e3aefd0e4819a7868c1ab6d977dea3f5c
juliomc23 <julito2302mc@gmail.com>
2022-05-30 09:14:47 +0200

    create file and delete

--- src/modules/renderfiles.php
+++ src/modules/renderfiles.php
@@ -116,17 +116,70 @@ function showTable($directories, $filesList, $rootUserPath)
           break;
       }
 
-      echo "<td><a href=" . $rootUserPath . str_replace(" ", "%20", $file) . " target=_blank>" . $infoFile['basename'] . "</a></td>";
+
+      echo '<td><a href=".' . $_SESSION['pathUser'] . str_replace(' ', '%20', $file) . '" target="_blank">' . $infoFile['basename'] . '</a></td>';
       echo '<td>' . date("m/d/y H:i A", filectime($rootUserPath . $file)) . '</td>';
       echo '<td>' . date("m/d/y H:i A", filemtime($rootUserPath . $file)) . '</td>';
       echo '<td>' . $extension . '</td>';
       echo '<td>' . formatSizeUnits(filesize($rootUserPath . $file))  . '</td>';
       echo '<td>' . date("m/d/y H:i A", fileatime($rootUserPath . $file)) . '</td>';
-      echo '<td>' . '<button class=""><i class="fa-solid fa-ellipsis"></i></button>' . '</td>';
+      echo '<td>' . '<a href="./index.php?path=.'.$_SESSION['pathUser'].'&file='.$infoFile['basename'].'&rename=true"><i class="fa-solid fa-pen-to-square"></i></a>' . '</td>';
+      echo '<td>' . '<a href="./index.php?path=.'.$_SESSION['pathUser'].'&file='.$infoFile['basename'].'&delete=true"><i class="fa-solid fa-trash"></i></a>' . '</td>';
 
       echo '</tr>';
     }
   }
   echo '</table>';
+
+  setFileInfo($infoFile);
+  deleteFile($infoFile);
+
 }
 
+function setFileInfo($infoFile){
+  if(isset($_GET['path']) && isset($_GET['rename'])){
+    echo '<form action="./index.php?path=.'.$_SESSION['pathUser'].'&file='.$infoFile['basename'].'" method="post">
+    <input type="text" name="new_name" placeholder="Introduce new file name" />
+    <input type="submit" value="Change">
+    </form>';
+  
+    renameFile();
+  }
+}
+
+
+// $oldfile = $_GET['path'] . $_GET['file'];
+//     $newfile = $_GET['path'] . $_POST['new_name'];
+
+//     var_dump($oldfile);
+//     var_dump($newfile);
+
+//     var_dump($_POST['new_name']);
+
+function renameFile(){
+
+  var_dump(isset($_GET['path']));
+  var_dump(isset($_GET['rename']));
+  var_dump(isset($_POST['new_name']));
+
+  if(isset($_GET['path'])&& isset($_GET['rename'])&& !isset($_POST['new_name'])){
+    // $oldfile = $_GET['path'] . $_GET['file'];
+    // $newfile = $_GET['path'] . $_POST['new_name'];
+
+    // var_dump($oldfile);
+    // var_dump($newfile);
+
+    // rename($oldfile, $newfile);
+
+    echo "Hola";
+  }
+}
+
+function deleteFile($file){
+
+  if(isset($_GET['path'])&& isset($_GET['delete'])){
+    unlink($_GET['path'].$file['basename']);
+
+    var_dump($_GET['path'], $file['basename'], $_GET['delete']);
+  }
+}
